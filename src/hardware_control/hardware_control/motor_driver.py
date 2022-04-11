925ba85c70c57cce8aeb1ea90b55ac8e73d620dd
Adam-Arezza <arezza.adam@gmail.com>
2022-04-11 02:12:33 +0000

    Modified motor_driver to use RPi.GPIO instead of serial comms

--- src/hardware_control/hardware_control/motor_driver.py
+++ src/hardware_control/hardware_control/motor_driver.py
@@ -1,37 +1,52 @@
-import serial
 import time
 import math
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import String
-from geometry_msgs.msg import Twist
+import RPi.GPIO as GPIO
 
 class Motor_driver(Node):
     def __init__(self):
         super().__init__("motor_driver")
         self.node_name = "motor_driver"
-        self.port = "/dev/ttyUSB1"
-        self.ser = serial.Serial(self.port, 115200, timeout=1)
+
+        # l293d enable pins 40,38
+        # l293d input pins 31,33,35,37
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(37, GPIO.OUT)
+        GPIO.setup(35, GPIO.OUT)
+        GPIO.setup(33, GPIO.OUT)
+        GPIO.setup(31, GPIO.OUT)
+        GPIO.setup(40, GPIO.OUT)
+        GPIO.setup(38, GPIO.OUT)
+
+        # start with motor driver disabled
+        GPIO.output(40, False)
+        GPIO.output(38, False)
 
         # Parameters
-        self.declare_parameter("pwm_min")
-        self.declare_parameter("pwm_max")
+        self.declare_parameter("dc_min")
+        self.declare_parameter("dc_max")
         self.declare_parameter("rad_min")
         self.declare_parameter("rad_max")
         self.rad_max = self.get_parameter("rad_max").get_parameter_value().double_value
         self.rad_min = self.get_parameter("rad_min").get_parameter_value().double_value
-        self.pwm_min = self.get_parameter("pwm_min").get_parameter_value().integer_value
-        self.pwm_max = self.get_parameter("pwm_max").get_parameter_value().integer_value
+        self.dc_min = self.get_parameter("dc_min").get_parameter_value().integer_value
+        self.dc_max = self.get_parameter("dc_max").get_parameter_value().integer_value
 
         # Subscribers and Publishers
         self.wheel_vels = self.create_subscription(Float32MultiArray, "/velocity", self.wheel_speeds, qos_profile=10)
         self.motor_pwms = self.create_subscription(Float32MultiArray, "/pidR_pidL", self.send_motor_commands, qos_profile=10)
-        self.pwm_publisher = self.create_publisher(String, "/pwmL_pwmR", qos_profile=10)
+        self.dc_publisher = self.create_publisher(String, "/dcL_dcR", qos_profile=10)
 
         # Variables
         self.right_wheel = 0.0
         self.left_wheel = 0.0
+        self.left_duty_cycle = 0
+        self.right_duty_cycle = 0
 
         #Initialization message
         self.get_logger().info(f'{self.node_name} is now online.')
@@ -42,7 +57,7 @@ class Motor_driver(Node):
     
     def scale_vals(self, pid, wheel_speed):
         rad_range = self.rad_max - self.rad_min
-        pwm_range = self.pwm_max - self.pwm_min
+        dc_range = self.dc_max - self.dc_min
         rad_per_sec = pid + wheel_speed
         
         if pid == 0:
@@ -51,21 +66,21 @@ class Motor_driver(Node):
 
         if rad_per_sec < 0:
             rps = -1 * rad_per_sec
-            new_rps = (((rps - 0) * pwm_range) / rad_range) + self.pwm_min
+            new_rps = (((rps - 0) * dc_range) / rad_range) + self.dc_min
             output = new_rps * -1
 
         if rad_per_sec > 0:
-            new_rps = (((rad_per_sec - 0) * pwm_range) / rad_range) + self.pwm_min
+            new_rps = (((rad_per_sec - 0) * dc_range) / rad_range) + self.dc_min
             output = new_rps
 
         if rad_per_sec == 0:
             output = 0
 
-        if output > self.pwm_max:
-            output = self.pwm_max
+        if output > self.dc_max:
+            output = self.dc_max
 
-        if output < -self.pwm_max:
-            output = -self.pwm_max
+        if output < -self.dc_max:
+            output = -self.dc_max
 
         # if output > 0 and output < self.pwm_min:
         #     output = self.pwm_min
@@ -77,12 +92,11 @@ class Motor_driver(Node):
     def send_motor_commands(self, pid_vals):
         pid_right = pid_vals.data[0]
         pid_left = pid_vals.data[1]
-        pwmL = str(self.scale_vals(pid_left, self.right_wheel))
-        pwmR = str(self.scale_vals(pid_right, self.left_wheel))
-        msg = f'<{pwmR}:{pwmL}>'
+        dcL = str(self.scale_vals(pid_left, self.right_wheel))
+        dcR = str(self.scale_vals(pid_right, self.left_wheel))
+        msg = f'{dcR}:{dcL}'
         pwm_msg = String()
         pwm_msg.data = msg
-        self.ser.write(msg.encode('ascii'))
         self.pwm_publisher.publish(pwm_msg)
 
 def main(args=None):
